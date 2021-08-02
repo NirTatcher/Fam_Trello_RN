049e0d7a60243593bccf898fa6e83f5727957229
Nir <nirto111@gmail.com>
2021-08-02 20:18:28 +0300

    working on editing

--- pages/AllNotes.js
+++ pages/AllNotes.js
@@ -1,12 +1,136 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+import { ListItem, Avatar } from 'react-native-elements';
+import { Button, Overlay, Icon } from 'react-native-elements';
+import Note_Overlay from './Note_Overlay';
+import AppLoading from 'expo-app-loading';
 
-export default function AllNotes({props,route,navigation}) {
-    return (
-        <View>
-            <Text>All Notes</Text>
-            {console.log(route.params.note)}
-            {/* <Text>{route.params}</Text> */}
-        </View>
-    )
+
+export default function AllNotes({ props, route, navigation }) {
+
+    const [visible, setVisible] = useState(false);
+    const [current, setCurrent] = useState(0);
+    const [fam_notes, setFamNotes] = useState(undefined)
+    const [curret_user_notes, setCurrentUserNotes] = useState([])
+
+    useEffect(() => {
+        // {  console.log(route.params.fam_notes)}
+        setFamNotes(route.params.fam)
+        // setFamNotes(route.params.curret_user_notes)
+        console.log(route.params.user);
+      setCurrentUserNotes(route.params.user)
+        // return () => {
+        //     cleanup
+        // }
+    }, [])
+
+    // useEffect(() => {
+    //     // {  console.log(route.params.fam_notes)}
+    //     setFamNotes(route.params.fam)
+    //     // setFamNotes(route.params.curret_user_notes)
+    //     console.log(route.params.user);
+    //   setCurrentUserNotes(route.params.user)
+    //     // return () => {
+    //     //     cleanup
+    //     // }
+    // }, [fam_notes,curret_user_notes])
+    
+
+    const toggleOverlay = (e) => {
+        if (visible === false)
+            setCurrent(e)
+        else
+            setCurrent(null)
+        setVisible(!visible);
+    }
+
+
+
+
+
+    if (fam_notes === undefined || curret_user_notes===undefined) {
+        return <AppLoading />
+    }
+    else
+        return (
+            <ScrollView>
+                <View>
+                    <Text>All Notes</Text>
+                    {/* {console.log(route.params.fam_notes)}
+                
+                {/* <Text>{route.params}</Text> */}
+                  
+                    {
+                        fam_notes?.map((l, i) =>
+                            // notes.map((l, i) =>0
+                            curret_user_notes?.find(n => n.title === l.title) !== undefined ?
+                                (<View key={i}>
+
+                                    <TouchableOpacity key={i} onPress={() => toggleOverlay(i)}>
+                                        <ListItem
+                                            key={i} bottomDivider>
+                                            <ListItem.Content>
+                                                <ListItem.Title>{l.title}
+                                                    <Icon
+                                                        name="edit"
+                                                        color="black"
+                                                        onPress={() => navigation.navigate("EditNote", { note: l })}
+                                                    />
+                                                    <Icon
+                                                        onPress={async() => 
+                                                            
+                                                             
+                                                        {    
+                                                            // setFamNotes(fam_notes.filter(n => n.id !== id))
+                                                           route.params.delete(l.id)}
+                                                         
+                                                        
+                                                        }
+                                                        name="delete"
+                                                        color="black"
+                                                    />
+                                                </ListItem.Title>
+                                                <ListItem.Subtitle>{l.text}</ListItem.Subtitle>
+                                                <ListItem.Subtitle>{l.users_tagged}</ListItem.Subtitle>
+
+                                            </ListItem.Content>
+                                            <ListItem.Chevron />
+                                        </ListItem>
+                                    </TouchableOpacity >
+
+                                </View>)
+                                :
+                                (<View key={i}>
+                                    <TouchableOpacity key={i} onPress={() => toggleOverlay(i)}>
+                                        <ListItem key={i} bottomDivider>
+                                            {/* <Avatar source={{ uri: l.avatar_url }} /> */}
+                                            <ListItem.Content>
+
+                                                <ListItem.Title><Text>{l.title}</Text></ListItem.Title>
+                                                <ListItem.Subtitle><Text>{l.text}</Text></ListItem.Subtitle>
+                                                <ListItem.Subtitle><Text>{l.users_tagged}</Text></ListItem.Subtitle>
+
+                                            </ListItem.Content>
+                                            <ListItem.Chevron />
+                                        </ListItem>
+                                    </TouchableOpacity >
+
+                                </View>)
+                        )
+                    }
+                    <Overlay isVisible={visible} onBackdropPress={() => toggleOverlay(current)}>
+
+                        <TouchableOpacity key={current} style={{ alignSelf: 'flex-end' }} onPress={() => toggleOverlay(current)}>
+                            <ListItem.Chevron />
+                        </TouchableOpacity>
+                        <Note_Overlay note={current ? fam_notes[current] : null} />
+
+                    </Overlay>
+
+
+
+                </View>
+            </ScrollView>
+        )
 }
--- pages/DrawerNavManager.js
+++ pages/DrawerNavManager.js
@@ -12,7 +12,7 @@ import AppLoading from 'expo-app-loading';
 const Drawer = createDrawerNavigator();
 export default function DrawerNavManager({route,navigation}) {
     const [fam_notes, setFamNotes] = useState(undefined)
-    const [curret_user_notes, setCurrentUserNotes] = useState([])
+    const [curret_user_notes, setCurrentUserNotes] = useState(undefined)
     const [username, setUser] = useState("david22")
     const [visible, setVisible] = useState(false);
     const [current, setCurrent] = useState(0);
@@ -24,12 +24,60 @@ export default function DrawerNavManager({route,navigation}) {
 
 
 
-       await fetchFamNotes(urlFamNotes)
+        fetchFamNotes(urlFamNotes)
 
-        // fetchUsersNotes(urlCurrentNotes)
+       await fetchUsersNotes(urlCurrentNotes)
         // return () => {
         // }
     }, [])
+
+
+    useEffect(() => {
+        let urlCurrentNotes = "http://ruppinmobile.tempdomain.co.il/site09/api/Note/fam_member/" + fam_ID + "/" + username;
+        fetchUsersNotes(urlCurrentNotes)
+
+    }, [curret_user_notes])
+
+
+
+    useEffect(() => {
+        let urlFamNotes = "http://ruppinmobile.tempdomain.co.il/site09/api/Note/family/cohen222";
+        fetchFamNotes(urlFamNotes)
+
+    }, [fam_notes])
+
+    const deleteNote = (id) => {
+         console.log(id)
+        let url = "http://ruppinmobile.tempdomain.co.il/site09/api/Note/" + id;
+
+        fetch(url,
+            {
+                method: 'DELETE',
+                headers: new Headers({
+                    'accept': 'application/json; charset=UTF-8'
+                })
+
+            }
+
+        ).then(
+            res => {
+               
+                return res.json()
+            }
+        ).then(
+           async (result) => {
+                alert("Success")
+                let fam_notes_temp = fam_notes
+                await fam_notes_temp.filter(n => n.id !== id)
+                setFamNotes(fam_notes_temp)
+                
+            },
+            (error) =>
+                alert(error)
+
+        )
+    }
+
 
     const fetchFamNotes = (urlFamNotes) => {
         fetch(urlFamNotes, {
@@ -50,7 +98,6 @@ export default function DrawerNavManager({route,navigation}) {
         ).then(
             (result) => {
                 // setFamNotes(result)
-                console.log("IM HERE");
                 setFamNotes(result)
 
             },
@@ -91,20 +138,55 @@ export default function DrawerNavManager({route,navigation}) {
             }
         )
     }
-    if(fam_notes===undefined){
+    const addingNote = async (note) => {
+
+
+        await fetch('http://ruppinmobile.tempdomain.co.il/site09/api/Note/',
+            {
+                method: 'POST',
+                body: JSON.stringify(note),
+                headers: new Headers({
+                    'Content-type': 'application/json; charset=UTF-8'
+                })
+
+            }
+
+        ).then(
+            res => {
+
+                return res.json()
+            }
+        ).then(
+            async (result) => {
+
+            },
+            (error) =>
+                console.log("")
+
+        )
+        let notesCurrent = curret_user_notes
+        let notesFam = fam_notes
+        notesCurrent.push(note)
+        notesFam.push(note)
+        await setCurrentUserNotes(notesCurrent)
+        await setFamNotes(notesFam)
+
+
+
+    }
+    if(fam_notes===undefined||curret_user_notes===undefined){
          return <AppLoading/>
     }
     else
     return (
     
     <Drawer.Navigator initialRouteName="All">
-        {/* {console.log(fam_notes)} */}
         <Drawer.Screen
             name="All"
             component={AllNotes}
             
-            initialParams={{ hello:{id:'5',value:8},note:fam_notes}}
-            options={{ drawerLabel: 'All' }}
+            initialParams={{ hello:{id:'5',value:8},fam:fam_notes,user:curret_user_notes,delete:(i)=>{deleteNote(i)}}}
+            options={{ drawerLabel: 'All', }}
         />
   
   <Drawer.Screen
